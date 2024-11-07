242e6c0224fd641d8c5f8d95c8516415e6cbfafa
Myaroshev <63793985+Myaroshev@users.noreply.github.com>
2024-11-08 02:53:05 +0500

    Add files via upload

---
+++ PL_lab3/Class1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PL_lab3
+{
+    internal class Class1
+    {
+        int[,] array;
+
+        //конструктор_1
+        public Class1(int n, int m)
+        {
+            array = new int[n, m];
+
+            if (n <= 0 || m <= 0)
+            {
+                throw new ArgumentException("Размер матрицы должен быть больше нуля\n");
+            }
+
+            Console.WriteLine("Введите элементы матрицы по столбцам:");
+            for (int j = 0; j < m; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write($"[{i},{j}]: ");
+                    while (!int.TryParse(Console.ReadLine(), out array[i, j]))
+                    {
+                        Console.WriteLine("Неверный ввод! Введите число");
+                        Console.Write($"[{i},{j}]: ");
+                    }
+                }
+            }
+        }
+
+        //конструктор_2
+        public Class1(int n)
+        {
+            array = new int[n, n];
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Размер матрицы должен быть больше нуля");
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    array[i, j] = Generator(n);
+                }
+            }
+        }
+
+        //конструктор_3
+        public Class1(short n, short n1, short n2)
+        {
+
+            n = 5;
+            if (n <= 0)
+            {
+                Console.WriteLine("Размер матрицы должен быть больше нуля");
+            }
+
+            array = new int[n, n];
+            int k = 1;//счётчик для диагонали
+            int current = n - 1;//для змейки
+
+            //заполнение главной диагонали
+            for (int i = 0; i < n; i++)
+            {
+                array[i, i] = k++;
+            }
+
+            for (int i = 0; i < n-1; i++) 
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (current >= 1)
+                    {
+                        array[i, j] = current--;
+                    }
+                }
+                current = n - 1 - i - 1;
+                if (current < 1)
+                {
+                    current = 1;
+                }
+            }
+
+        }
+
+        //заполнение конструктора_2
+        public int Generator(int n) 
+        {
+            //n - заглушка
+            Random random = new Random();
+            int number = 0;
+
+            number = random.Next(1, 5) * 2;
+
+            for (int i = 1; i < 4; i++)
+            {
+                number = number * 10 + random.Next(0, 5) * 2;
+            }
+
+            return number;
+        }
+
+        //вывод массива
+        public void Print()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+
+
+        //задание_4
+        // Метод для генерации и заполнения бинарного файла случайными данными
+        public static void generateFile(string filePath, int size)
+        {
+            Random random = new Random();
+
+            using (BinaryWriter writer = new BinaryWriter(new FileStream(filePath, FileMode.Create)))
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    int randomNumber = random.Next(0, 50);
+                    writer.Write(randomNumber);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ PL_lab3/Program.cs
@@ -0,0 +1,173 @@
+﻿using System.ComponentModel.Design;
+
+namespace PL_lab3
+{
+    internal class Program
+    {
+        static void menu() 
+        {
+            Console.WriteLine("1. Задание №1");
+            Console.WriteLine("2. Задание №2");
+            Console.WriteLine("3. Задание №3");
+            Console.WriteLine("4. Задание №4");
+        }
+
+        static void Main(string[] args)
+        {
+            while(true) 
+            {
+                menu(); 
+
+                Console.Write("\nВыберите действие: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch(choice) 
+                {
+                    case 1:
+                        Console.Clear();
+
+                        while (true) 
+                        {
+                            Console.Write("Введите число строк (n):");
+                            string n_input = Console.ReadLine();
+
+                            if (n_input == "0")
+                            {
+                                Console.Clear();
+                                break;
+                            }
+
+                            Console.Write("Введите число столбцов (m):");
+                            string m_input = Console.ReadLine();
+
+
+
+                            int n;
+                            if (!int.TryParse(n_input, out n))
+                            {
+                                Console.WriteLine("Введите число!");
+                                continue;
+                            }
+                            int m;
+                            if (!int.TryParse(m_input, out m))
+                            {
+                                Console.WriteLine("Введите число!");
+                                continue;
+                            }
+
+
+
+                            try
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("-----#Первый массив#-----");
+                                Class1 matrix1 = new Class1(n, m);
+                                matrix1.Print();
+                                Console.WriteLine();
+
+
+                                Console.WriteLine("-----#Второй массив#-----");
+                                Class1 matrix2 = new Class1(n);
+                                matrix2.Print();
+                                Console.WriteLine();
+
+                                short new_n = (short)n;
+                                Console.WriteLine("-----#Третий массив#-----");
+                                Class1 matrix3 = new Class1(new_n, new_n, new_n);
+                                matrix3.Print();
+                                Console.WriteLine();
+
+
+
+                                break;
+                            }
+                            catch (ArgumentException ex)
+                            {
+                                Console.WriteLine($"{ex.Message}");
+                            }
+
+                        }
+
+                        break;
+
+
+                    case 2:
+                        Console.Clear();
+
+                        while (true)
+                        {
+                            string sourceFile = "source.bin";
+
+                            Console.Write("Введите размер файла:");
+                            string file_size = Console.ReadLine();
+
+                            if (file_size == "0")
+                            {
+                                Console.Clear();
+                                break;
+                            }
+
+
+                            int n;
+                            if (!int.TryParse(file_size, out n))
+                            {
+                                Console.WriteLine("Введите число!");
+                                continue;
+                            }
+
+                            try
+                            {
+                                //генерация бинарного файла
+                                Class1.generateFile(sourceFile, n);
+                                Console.WriteLine($"Исходный файл '{sourceFile}' создан");
+
+                                Console.WriteLine();
+
+                                using (BinaryReader reader = new BinaryReader(File.Open(sourceFile, FileMode.Open)))
+                                {
+                                    while (reader.PeekChar() != -1) 
+                                    {
+                                        int number = reader.ReadInt32(); 
+                                        Console.Write($"{number} ");
+                                    }
+                                    Console.WriteLine();
+                                }
+                                Console.WriteLine();
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"{ex.Message}");
+                            }
+                        }
+
+
+                        break;
+
+                }
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
