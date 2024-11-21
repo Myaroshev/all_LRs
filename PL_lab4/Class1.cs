3ed80ba37fcbfdf19a261468f3cb8349c9781c7e
Myaroshev <63793985+Myaroshev@users.noreply.github.com>
2024-11-22 02:29:32 +0500

    Add files via upload

---
+++ PL_lab4/Class1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+
+
+namespace PL_lab4
+{
+
+    [Serializable]
+    internal class Class1<T>
+    {
+
+
+        //задание_№1
+        public static bool isElements(List<T> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[i].Equals(list[j]))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+
+
+
+        //задание_№2
+        public static void remove_items<T>(LinkedList<T> list, T value, string direction)
+        {
+            if (direction.ToLower() == "start")
+            {
+                LinkedListNode<T> node = list.First;
+                while (node != null)
+                {
+                    if (node.Value.Equals(value))
+                    {
+                        list.Remove(node);
+                        return;
+                    }
+                    node = node.Next;
+                }
+
+                Console.WriteLine("Элемент не найден в списке");
+            }
+            else if (direction.ToLower() == "end")
+            {
+                LinkedListNode<T> node = list.Last;
+                while (node != null)
+                {
+                    if (node.Value.Equals(value))
+                    {
+                        list.Remove(node);
+                        Console.WriteLine($"Элемент {value} удален из списка");
+                        return;
+                    }
+                    node = node.Previous;
+                }
+
+                Console.WriteLine("Элемент не найден в списке");
+            }
+            else
+            {
+                Console.WriteLine("Используйте 'start' или 'end'");
+            }
+        }
+
+
+
+
+
+
+
+
+
+        //задание_№3
+        public static HashSet<T> all_likes<T>(HashSet<T> musics, HashSet<T> meloman1, HashSet<T> meloman2, HashSet<T> meloman3)
+        {
+            HashSet<T> all_music_liked = new HashSet<T>(musics); //в этом множестве находятся ВСЕ произведения
+
+            //пересечение
+            //например: есть Произведение_1, Произведение_2, Произведение_3, Произведение_4, Произведение_5
+            //и у второго - Произведение_1, Произведение_2, Произведение_3, Произведение_4
+            //останется в новом HashSet Произведение_1, Произведение_2, Произведение_3, Произведение_4
+            //потом это сверится с новым у meloman_2 - "Произведение 2", "Произведение 3" и останется Произведение_2, Произведение_3
+            //и с последним meloman_3 - "Произведение 1", "Произведение 3", "Произведение 4" и останется Произведение_3(которое нравится всем)
+
+            all_music_liked.IntersectWith(meloman1);
+            all_music_liked.IntersectWith(meloman2);
+            all_music_liked.IntersectWith(meloman3);
+
+            return all_music_liked;
+        }
+
+        public static HashSet<T> someone_likes<T>(HashSet<T> meloman1, HashSet<T> meloman2, HashSet<T> meloman3)
+        {
+            HashSet<T> someone_liked = new HashSet<T>();//пустое множество
+
+            //объединение
+            //например Произведение_1, Произведение_2, Произведение_3, Произведение_4 у meloman_1 и 0 у someone_liked
+            //при объединении someone_liked получит в себя с 1-4 произведения, если будут новые, то он также их в себя добавит
+
+            someone_liked.UnionWith(meloman1);
+            someone_liked.UnionWith(meloman2);
+            someone_liked.UnionWith(meloman3);
+
+            return someone_liked;
+        }
+
+        public static HashSet<T> none_likes<T>(HashSet<T> musics, HashSet<T> someone_liked)
+        {
+            HashSet<T> none_liked = new HashSet<T>(musics); //множество со всеми типами произведений
+
+            //грубо говоря - разность. Мы берем произведения, которые нравятся некоторым и вычитаем из всего списка эти произведения
+            //разность - произведения, которые никому не нравятся
+
+            none_liked.ExceptWith(someone_liked);
+
+            return none_liked;
+        }
+
+
+
+
+
+
+
+
+
+        //задание_№4
+        public static HashSet<char> unique_letters(string file_path)
+        {
+            HashSet<char> unique_letters = new HashSet<char> { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };//гласные буквы
+
+            HashSet<char> unique_letters_exist_1 = new HashSet<char>();//гласные, которые встречаются раз
+            HashSet<char> letters_exist_1_more = new HashSet<char>(); //гласные, которые встречаются более чем в одном слове
+
+
+            using (StreamReader reader = new StreamReader(file_path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var words = line.Split(new[] { ' ', '\n', ',', '.', '!', '?', ';', ':', '-' }, StringSplitOptions.RemoveEmptyEntries);
+                    //разбив строки на отдельные слова "Привет! Как дела?" -> "Привет", "Как", "дела"
+
+                    foreach (var word in words)
+                    {
+                        HashSet<char> lower_word = new HashSet<char>();//будет содержать только уникальные символы из текущего слова
+
+                        foreach (char ch in word.ToLower())
+                        {
+                            if (unique_letters.Contains(ch))//содержится ли текущий символ в наборе уникальных букв
+                            {
+                                lower_word.Add(ch); //символ найден в наборе, добавляем его в множество lower_word
+                            }
+                        }
+
+                        //если найдены гласные, добавляем их в уникальные и повторяющиеся
+                        if (lower_word.Count > 0)
+                        {
+                            //добавляем найденные гласные в уникальные
+                            foreach (var letter in lower_word)
+                            {
+                                //если эта гласная уже встречалась в другом слове, помечаем её как повторяющуюся и записываем во множество
+                                if (unique_letters_exist_1.Contains(letter))
+                                {
+                                    letters_exist_1_more.Add(letter);
+                                }
+                                else
+                                {
+                                    unique_letters_exist_1.Add(letter);
+                                }
+                            }
+                        }
+                    }
+
+                    //удаляем гласные, которые встречаются более чем в одном слове из уникальных гласных
+                    foreach (var letters in letters_exist_1_more)
+                    {
+                        unique_letters_exist_1.Remove(letters);
+                    }
+                }
+
+            }
+
+
+            return unique_letters_exist_1; //возвращаем множество гласных
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //5 задание
+    }
+
+}
---
+++ PL_lab4/Program.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PL_lab4
+{
+    internal class Program
+    {
+        static void menu()
+        {
+            Console.WriteLine("1. Задание №1");
+            Console.WriteLine("2. Задание №2");
+            Console.WriteLine("3. Задание №3");
+            Console.WriteLine("4. Задание №4");
+            Console.WriteLine("5. Задание №5");
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                menu();
+
+                Console.Write("\nВыберите действие: ");
+                string choice = Console.ReadLine();
+
+                int n;
+                if (!int.TryParse(choice, out n))
+                {
+                    Console.WriteLine("Введите число!");
+                }
+                Console.WriteLine();
+
+
+
+                switch (n)
+                {
+                    case 1:
+                        Console.Clear();
+
+                        try 
+                        {
+                            //список типа int
+                            List<int> numbers = new List<int> { };
+                            Console.Write("Список типа 'int' (end - для завершения): ");
+                            while (true)
+                            {
+                                string input = Console.ReadLine();
+                                if (input.ToLower() == "end")
+                                {
+                                    break;
+                                }
+
+                                if (int.TryParse(input, out int number))
+                                {
+                                    numbers.Add(number);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Введите число/'end' для завершения");
+                                }
+                            }
+                            bool duplicates = Class1<int>.isElements(numbers);
+
+                            foreach (int i in numbers)
+                            {
+                                Console.Write($"{i} ");
+                            }
+                            Console.Write($"- Дублирующиеся элементы есть? - {duplicates}");
+
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+
+
+
+
+                            //список типа string
+                            List<string> strings = new List<string> { };
+                            Console.Write("Список типа 'string' (end - для завершения): ");
+                            while (true)
+                            {
+                                string input = Console.ReadLine();
+                                if (input.ToLower() == "end")
+                                {
+                                    break;
+                                }
+
+                                strings.Add(input);
+                            }
+
+
+                            bool duplicates_string = Class1<string>.isElements(strings);
+                            foreach (string i in strings)
+                            {
+                                Console.Write($"{i} ");
+                            }
+                            Console.Write($"- Дублирующиеся элементы есть? - {duplicates_string}");
+
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+
+
+
+
+                            //список типа double
+                            List<double> numbers_two = new List<double> { };
+                            Console.Write("Список типа 'double' (end - для завершения): ");
+                            while (true)
+                            {
+                                string input = Console.ReadLine();
+                                if (input.ToLower() == "end")
+                                {
+                                    break;
+                                }
+
+                                if (double.TryParse(input, out double number))
+                                {
+                                    numbers_two.Add(number);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Введите число/'end' для завершения");
+                                }
+                            }
+
+                            bool duplicates_two = Class1<double>.isElements(numbers_two);
+                            foreach (double i in numbers_two)
+                            {
+                                Console.Write($"{i} ");
+                            }
+                            Console.Write($"- Дублирующиеся элементы есть? - {duplicates_two}");
+
+                            Console.WriteLine();
+                            Console.WriteLine();
+                        }
+                        catch(Exception e) 
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        
+
+                        break;
+
+
+
+
+
+                    case 2:
+                        Console.Clear();
+
+                        try 
+                        {
+
+                            LinkedList<double> L = new LinkedList<double>();
+
+                            while (true) 
+                            {
+                                Console.WriteLine("Введите число для добавления в список или 'end' для завершения:");
+                                string input = Console.ReadLine();
+
+                                if (input.ToLower() == "end")
+                                {
+                                    break;
+                                }
+
+                                if (double.TryParse(input, out double number))
+                                {
+                                    Console.WriteLine("Добавить в f/F(начало) или l/L(конец) списка?");
+                                    string position = Console.ReadLine();
+
+                                    if (position == "f" || position == "F")
+                                    {
+                                        L.AddFirst(number);
+                                    }
+                                    else if (position == "l" || position == "L")
+                                    {
+                                        L.AddLast(number);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Ошибка ввода. Число добавлено в конец");
+                                        L.AddLast(number);
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Вводите только числа или 'end' для выхода");
+                                }
+
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine("Список:");
+                            foreach (var item in L)
+                            {
+                                Console.Write($"{item} ");
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.Write("Введите элемент, который хотите удалить:");
+                            string n_input = Console.ReadLine();
+                            double n2;
+                            if (!double.TryParse(n_input, out n2))
+                            {
+                                Console.WriteLine("Введите число!");
+                                continue;
+                            }
+
+                            Console.WriteLine();
+
+                            Console.Write("Введите направление списка:");
+                            string direction = Console.ReadLine();
+
+                            Class1<double>.remove_items(L, n2, direction);
+
+                            Console.WriteLine();
+                            Console.WriteLine("Новый список:");
+                            foreach (var item in L)
+                            {
+                                Console.Write($"{item} ");
+                            }
+
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine();
+
+                        break;
+
+
+
+
+
+                    case 3:
+                        Console.Clear();
+
+                        try
+                        {
+                            //все произведения
+                            HashSet<string> musics = new HashSet<string>
+                            {
+                                "Произведение 1",
+                                "Произведение 2",
+                                "Произведение 3",
+                                "Произведение 4",
+                                "Произведение 5"
+                            };
+
+                            //музыка у меломанов
+                            HashSet<string> meloman_1 = new HashSet<string> { "Произведение 1", "Произведение 2", "Произведение 3", "Произведение 4" };
+                            HashSet<string> meloman_2 = new HashSet<string> { "Произведение 2", "Произведение 3" };
+                            HashSet<string> meloman_3 = new HashSet<string> { "Произведение 1", "Произведение 3", "Произведение 4" };
+
+                            //нравятся всем
+                            HashSet<string> all_music_liked = Class1<string>.all_likes(musics, meloman_1, meloman_2, meloman_3);
+
+                            //нравятся некоторым
+                            HashSet<string> someone_liked = Class1<string>.someone_likes(meloman_1, meloman_2, meloman_3);
+
+                            //никому не нравятся
+                            HashSet<string> none_liked = Class1<string>.none_likes(musics, someone_liked);
+
+                            Console.WriteLine($"Нравятся всем: {string.Join(", ", all_music_liked)}");
+                            Console.WriteLine($"Нравятся некоторым: {string.Join(", ", someone_liked)}");
+                            Console.WriteLine($"Никому не нравятся: {string.Join(", ", none_liked)}");
+
+
+
+                        }
+                        catch (Exception e)
+                        {
+
+                            Console.WriteLine(e.Message);
+
+
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine();
+
+                        break;
+
+
+
+
+
+
+
+
+
+
+                    case 4:
+                        Console.Clear();
+
+                        try
+                        {
+                            string source_file = "source_file2.txt";
+                            //string source_file = "source_file.txt";
+
+                            HashSet<char> unique_letters = Class1<string>.unique_letters(source_file);
+
+                            Dictionary<char, int> letters_dict = new Dictionary<char, int>
+                            {
+                                { 'а', 1 },
+                                { 'е', 2 },
+                                { 'ё', 3 },
+                                { 'и', 4 },
+                                { 'о', 5 },
+                                { 'у', 6 },
+                                { 'ы', 7 },
+                                { 'э', 8 },
+                                { 'ю', 9 },
+                                { 'я', 10 }
+                            };
+
+
+                            Console.WriteLine("Гласные буквы, которые не входят более чем в одно слово:");
+                            var sorted_letters = unique_letters.Where(letters_dict.ContainsKey).OrderBy(letter => letters_dict[letter]);
+                            //Where - проверяем есть ли буква в словаре
+                            //orderby - сортировка по возрастанию
+
+                            foreach (var letter in sorted_letters)
+                            {
+                                Console.WriteLine(letter);
+                            }
+
+
+
+                        }
+                        catch (Exception e)
+                        {
+
+                            Console.WriteLine(e.Message);
+
+
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine();
+
+                        break;
+
+
+                }
+
+
+
+            }
+
+        }
+    }
+}
