f5f91420c81b9ff70409b3a118ee0d86ffca7ba7
Myaroshev <63793985+Myaroshev@users.noreply.github.com>
2024-11-22 14:28:01 +0500

    Update Class1.cs

--- PL_lab3/Class1.cs
+++ PL_lab3/Class1.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PL_lab3
 {
